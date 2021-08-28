a3c95a747734c0ac87001cd68162b21cb3a44e56
kalaivanan241 <kalaivanan241@gmail.com>
2021-08-28 22:06:33 +0800

    setup basic strapi with next js

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '567577d99b516bf0f6669c0de7995872'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '41a3037a-5bd4-4f37-bd91-f7edbaf50b73'
+};
